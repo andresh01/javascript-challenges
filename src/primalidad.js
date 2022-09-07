fb4154f894224e0959cd82875b363148af487af4
andresh01 <andres85684@outlook.com>
2022-09-06 22:41:42 -0500

    Ejercicios resueltos de fibonacci,numeros primos y factorial

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,21 @@
+//funcion que recibe un numero y devuleve si es primo o no
 const trialDivision = (number) => {
-  // your code here
+  let counter = 0;
+  let module = 0;
+
+//ciclo que recorre desde el numero ingresado hasta uno para realizarle el modulo
+  for (let j = 1; j <= number; j++) { 
+    module = number % j;
+
+    if (module == 0) { //si el modulo es igual a cero quiere decir que es divisible por el numero y aumneto el contador en uno
+      counter++;
+    }
+
+  }
+  if (counter == 2) { //si solo se encontraron dos numeros con modulo cero entonces es primo
+    return `${number} es primo`;
+  } else { return `${number} NO es primo`; }
+
 }
 
 module.exports = trialDivision;
